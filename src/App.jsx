6b5247168b81eaa5a7c5ca8b0e37648190f512a7
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-24 12:16:45 +0000

    Added apiKey

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
 	async function getTokenBalance() {
 		const config = {
-			apiKey: '<-- COPY-PASTE YOUR ALCHEMY API KEY HERE -->',
+			apiKey: 'sufd7iQIMIlROiEZyAG17IGvWjKl9470',
 			network: Network.ETH_MAINNET,
 		};
 
