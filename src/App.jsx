610fe1d3ec03ab59d796afd87cd6f7faddd68d16
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-27 20:04:42 +0000

    Create util folder and moved the connectWallet function

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import {
 } from '@chakra-ui/react';
 import { Alchemy, Network, Utils } from 'alchemy-sdk';
 import { useState } from 'react';
-
+import { connectWallet } from './util/interact.jsx';
 function App() {
 	const [userAddress, setUserAddress] = useState('');
 	const [isLoading, setIsLoading] = useState(false);
@@ -40,41 +40,6 @@ function App() {
 
 	//configuration
 	const minimumValueToShow = 0.0001;
-
-	const connectWallet = async () => {
-		if (window.ethereum) {
-			try {
-				const addressArray = await window.ethereum.request({
-					method: 'eth_requestAccounts',
-				});
-				const obj = {
-					status: '👆🏽 Write a message in the text-field above.',
-					address: addressArray[0],
-				};
-				return obj;
-			} catch (err) {
-				return {
-					address: '',
-					status: '😥 ' + err.message,
-				};
-			}
-		} else {
-			return {
-				address: '',
-				status: (
-					<span>
-						<p>
-							{' '}
-							🦊{' '}
-							<a target='_blank' href={`https://metamask.io/download.html`}>
-								You must install Metamask, a virtual Ethereum wallet, in your browser.
-							</a>
-						</p>
-					</span>
-				),
-			};
-		}
-	};
 
 	const connectWalletPressed = async () => {
 		const walletResponse = await connectWallet();
